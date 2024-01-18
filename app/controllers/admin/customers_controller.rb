cbbb9a2d89c8d62976207e1e2fd43e8728821888
itoriko <127394855+itoriko@users.noreply.github.com>
2024-01-18 08:17:22 +0000

    会員一覧作成

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -1,11 +1,17 @@
 class Admin::CustomersController < ApplicationController
   def index
-    
+    @customers = Customer.all.page(params[:page]).per(10)
   end
 
   def show
   end
 
   def edit
+  end
+
+  private
+
+  def customer_params
+    params.require(:customer).permit(:last_name, :first_name, :email, :is_deleted)
   end
 end
