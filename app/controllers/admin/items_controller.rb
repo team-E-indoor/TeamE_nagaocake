e4dfe68f212e2810f3d748a6d114ac0543a7e83f
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-19 04:45:30 +0000

    商品登録

--- app/controllers/admin/items_controller.rb
+++ app/controllers/admin/items_controller.rb
@@ -1,13 +1,34 @@
 class Admin::ItemsController < ApplicationController
-  def index
+  def new
+    @item = Item.new
+  end
+  
+  def create
+    @item = Item.new(item_params)
+    # rails active_storage:installのコマンドした。ジャンル作ったら@item.genre_id = 1消す
+    @item.genre_id = 1
+    if @item.save
+      redirect_to admin_item_path(@item.id)
+    else
+      render :new
+    end
   end
 
-  def new
+
+  def index
   end
 
   def show
   end
 
   def edit
+    @item = Item.find(params[:id])
   end
+  
+  private
+
+  def item_params
+    params.require(:item).permit(:genre_id, :name, :item_description, :price, :is_active, :image )
+  end
+
 end
--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,22 @@
 class Item < ApplicationRecord
+  has_many :cart_items, dependent: :destroy
+  has_many :order_details
+  # belongs_to :genre
+  has_one_attached :image
+  
+  def get_image(width, height)
+    unless image.attached?
+      file_path = Rails.root.join('app/assets/images/no_image.jpg')
+      image.attach(io: File.open(file_path), filename: 'default-image.jpg', content_type: 'image/jpeg')
+    end
+    image
+  end
+  
+  def with_tax_price
+    (price * 1.1).floor
+  end
+  
+  validates :name, presence: true
+  validates :item_description, presence: true
+  validates :price, presence: true
 end
