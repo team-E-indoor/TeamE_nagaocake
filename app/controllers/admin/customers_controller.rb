2c7899ef16b7d13fcc6725c0cd1c045feda49821
itoriko <127394855+itoriko@users.noreply.github.com>
2024-01-19 05:45:06 +0000

    会員情報編集画面

--- app/controllers/admin/customers_controller.rb
+++ app/controllers/admin/customers_controller.rb
@@ -8,6 +8,13 @@ class Admin::CustomersController < ApplicationController
   end
 
   def edit
+    @customer = Customer.find(params[:id])
+  end
+
+  def update
+    @customer = Customer.find(params[:id])
+    @customer.update(customer_params)
+    redirect_to admin_customer_path
   end
 
   private
