e77c65e8a0da3ff1d012dd0c9f07745f77442b27
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-20 05:12:29 +0000

    ジャンル機能と商品登録修正

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,7 +1,7 @@
 class Item < ApplicationRecord
   has_many :cart_items, dependent: :destroy
   has_many :order_details
-  # belongs_to :genre
+  belongs_to :genre
   has_one_attached :image
   
   def get_image(width, height)
