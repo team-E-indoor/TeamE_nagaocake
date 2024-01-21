6bdf0d996230735f39b16084429e8df9e0ee023b
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-21 08:35:57 +0000

    作成途中２

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -9,10 +9,13 @@ class Admin::OrdersController < ApplicationController
     @order = Order.find(params[:id])
     @order_details = @order.order_details
     @order.update(order_params)
-    if order.status == "confirm_payment"
+    if order.status == "payment_confirmation"
       @order_details.each do |order_detail|
         oder_detail.update(making_status: 1)
       end
+    end
+    if order.update(order_params)
+      @order_details.update_all(making_status: "制作待ち") if @order.status == "入金確認"
     end
     　redirect_to admin_order_path(order.id), notice: "変更を完了しました"
   end
--- app/models/order.rb
+++ app/models/order.rb
@@ -3,5 +3,14 @@ class Order < ApplicationRecord
   has_many :items, through: :order_details
   belongs_to :customer
   
+  validates :customer_id, presence: true
+  validates :post_code, presence: true
+  validates :address, presence: true
+  validates :name, presence: true
+  validates :shipping_cost, presence: true
+  validates :total_price, presence: true
+  validates :payment_method, presence: true
+  validates :status, presence: true
+  
   
 end
