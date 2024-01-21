f8ef88541a1d731056899faf4ed38b7261202d74
EC2 Default User <ec2-user@ip-172-31-39-161.ap-northeast-1.compute.internal>
2024-01-21 06:33:18 +0000

    作成途中

--- app/controllers/admin/orders_controller.rb
+++ app/controllers/admin/orders_controller.rb
@@ -1,4 +1,25 @@
 class Admin::OrdersController < ApplicationController
   def show
+    @order = Order.find(params[:id])
+    @order_status = @order.status
+    @order_details = @order.order_details
+  end
+  
+  def update
+    @order = Order.find(params[:id])
+    @order_details = @order.order_details
+    @order.update(order_params)
+    if order.status == "confirm_payment"
+      @order_details.each do |order_detail|
+        oder_detail.update(making_status: 1)
+      end
+    end
+    　redirect_to admin_order_path(order.id), notice: "変更を完了しました"
+  end
+  
+  private
+
+  def order_params
+    params.require(:order).permit(:customer_id, :post_code, :address, :name, :shipping_cost, :total_price, :payment_method, :status)
   end
 end
--- app/models/order.rb
+++ app/models/order.rb
@@ -1,2 +1,7 @@
 class Order < ApplicationRecord
+  has_many :order_details, dependent: :destroy
+  has_many :items, through: :order_details
+  belongs_to :customer
+  
+  
 end
