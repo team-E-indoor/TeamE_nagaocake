16e9ad32f422293433a7124d4f7ee61de80b5ecb
NakanishiShoko <129033329+nakashoo@users.noreply.github.com>
2024-01-26 05:50:15 +0000

    ログインサインアップ後のページ遷移変更

--- app/controllers/public/registrations_controller.rb
+++ app/controllers/public/registrations_controller.rb
@@ -10,6 +10,10 @@ class Public::RegistrationsController < Devise::RegistrationsController
     devise_parameter_sanitizer.permit(:sign_up, keys: [:last_name,:first_name,:last_name_kana,:first_name_kana,:post_code,:address,:phone_number])
   end
 
+   def after_sign_up_path_for(resource)
+     customers_my_page_path
+   end
+
 
   # If you have extra params to permit, append them to the sanitizer.
   # def configure_sign_up_params
--- app/controllers/public/sessions_controller.rb
+++ app/controllers/public/sessions_controller.rb
@@ -21,8 +21,10 @@ class Public::SessionsController < Devise::SessionsController
    protected
 
    def after_sign_in_path_for(resource)
-     items_path
+     root_path
    end
+   
+   
 
   def customer_state
     @customer = Customer.find_by(email: params[:customer][:email])
